05d6003985ef38102bd20c62ed73e4a0a9e56d8e
aprabou <ashwinprabou16@gmail.com>
2025-10-08 12:33:03 -0700

    Added ScrollToTop function to start at the top of each page.

--- client/App.tsx
+++ client/App.tsx
@@ -10,6 +10,7 @@ import Index from "./pages/Index";
 import Team from "./pages/Team";
 import Sponsors from "./pages/Sponsors";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/team" element={<Team />} />
